3156e512347bfa9530993c1a0a30efcb9db7e091
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2020-08-07 09:15:17 +0200

    feat: repository added

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from './user/user.module';
 import { IngredientModule } from './ingredient/ingredient.module';
 import { InsightModule } from './insight/insight.module';
 import { SharedModule } from './shared/shared.module';
+import { RepositoryModule } from './repository/repository.module';
 
 @Module({
-  imports: [DetectionModule, UserModule, IngredientModule, InsightModule, SharedModule],
+  imports: [DetectionModule, UserModule, IngredientModule, InsightModule, SharedModule, RepositoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
