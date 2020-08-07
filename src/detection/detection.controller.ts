da1134fd11112c7a0cada91240601792bc98c59b
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2020-08-07 13:48:24 +0200

    automl works

--- src/detection/detection.controller.ts
+++ src/detection/detection.controller.ts
@@ -17,12 +17,8 @@ export class DetectionController {
   @UseInterceptors(FileInterceptor('file'))
   async detect(@UploadedFile() file: InterceptedFile) {
     const filePath = this.storage.write(file)
-
-    // Performs label detection on the image file
-    // const [result] = await this.client.labelDetection(filePath);
-    // const labels = result.labelAnnotations;
-    // console.log('Labels:');
-    // labels.forEach(label => console.log(label.description));
+    
+    const prediction = await this.predict(file)
     const results = await this.detectorService.process(file)
     return { status: 'success', payload: results }
   }
@@ -31,23 +27,18 @@ export class DetectionController {
     // Construct request
     // params is additional domain-specific parameters.
     // score_threshold is used to filter the result
-    // const request = {
-    //   name: this.client.modelPath('elated-bebop-154812', location, modelId),
-    //   payload: {
-    //     image: {
-    //       imageBytes: file.buffer,
-    //     },
-    //   },
-    // };
+    const request = {
+      name: this.client.modelPath('68979267479', 'us-central1', 'ICN1137548133027282944'),
+      payload: {
+        image: {
+          imageBytes: file.buffer,
+        },
+      },
+    };
 
-    // const [response] = await this.client.predict(request);
-    //
-    // for (const annotationPayload of response.payload) {
-    //   console.log(`Predicted class name: ${annotationPayload.displayName}`);
-    //   console.log(
-    //     `Predicted class score: ${annotationPayload.classification.score}`
-    //   );
-    // }
+    const [response] = await this.client.predict(request);
+    const [prediction] = response.payload
+    return prediction.displayName
   }
 
 }
