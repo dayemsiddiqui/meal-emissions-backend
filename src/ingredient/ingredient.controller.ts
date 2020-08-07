2b207158a33891d5b224adb1e509753471f105af
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2020-08-07 14:12:27 +0200

    created ingredients endpoints

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('ingredient')
-export class IngredientController {}
+@Controller('ingredients')
+export class IngredientController {
+  @Get()
+  ingredients() {
+    return {
+      ingredients: []
+    }
+  }
+}
